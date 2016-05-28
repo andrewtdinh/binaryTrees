ed77d92c7dfdc58d97d6caa9e99ae38a43e67fa0
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-28 09:46:11 -0700

    WIP - recurse makeSubNode function

--- binTree.js
+++ binTree.js
@@ -3,11 +3,20 @@
 function Node(data){
   this.data = data;
   this.parent = null;
-  this.left = [];
-  this.right = [];
+  this.left = {};
+  this.right = {};
 }
 
 function Tree(data){
   var node = new Node(data);
   this._root = node;
+}
+
+function binaryTree(str){
+  var binTree = {};
+  function makeSubNode(obj, aStr, data, left, right){
+    if (aStr === ''){
+      return;
+    }
+  }
 }
