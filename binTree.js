c35ed76e5497296b6f8387ed00a333701278ab4e
Andrew Dinh <andrewdinh74@yahoo.com>
2016-05-31 22:18:08 -0700

    Comment out unnecessary lines

--- binTree.js
+++ binTree.js
@@ -1,16 +1,16 @@
 'use strict';
 
-function Node(data){
-  this.data = data;
-  this.parent = null;
-  this.left = {};
-  this.right = {};
-}
-
-function Tree(data){
-  var node = new Node(data);
-  this._root = node;
-}
+// function Node(data){
+//   this.data = data;
+//   this.parent = null;
+//   this.left = {};
+//   this.right = {};
+// }
+//
+// function Tree(data){
+//   var node = new Node(data);
+//   this._root = node;
+// }
 
 function binaryTree(str){
   // var binTree = {};
