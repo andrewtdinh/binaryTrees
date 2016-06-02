37e5020525db8ec2e0ab7e0c15c6b5970480e3f5
Andrew Dinh <andrewdinh74@yahoo.com>
2016-06-02 10:26:16 -0700

    Deleting some comments

--- binTree.js
+++ binTree.js
@@ -1,17 +1,5 @@
 'use strict';
 
-// function Node(data){
-//   this.data = data;
-//   this.parent = null;
-//   this.left = {};
-//   this.right = {};
-// }
-//
-// function Tree(data){
-//   var node = new Node(data);
-//   this._root = node;
-// }
-
 function binaryTree(str){
   var binTree = {};
   // var ptr = 0;
